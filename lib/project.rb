7752569d978b4e2a9a155b98b118b5a7727a13f5
Asija Watson <asija.w@yahoo.com>
2020-01-19 04:44:45 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless back_project.backed_projects.include?(self)
+    #backer.back_project(self) unless back_project.backed_projects.include?(self)
   end 
 end
